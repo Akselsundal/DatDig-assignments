07b242dc75d9d5c30a62d0f8441e42b0fd45813a
Aksel Sundal <aksel.sundal@gmail.com>
2019-10-28 22:36:46 +0100

    ferdig øving2!

---
+++ o2/GNU ARM v7.2.1 - Release/build/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../build/bsp_stk.o \
+../build/bsp_stk_leds.o \
+../build/bsp_trace.o \
+../build/em_assert.o \
+../build/em_cmu.o \
+../build/em_emu.o \
+../build/em_gpio.o \
+../build/em_lcd.o \
+../build/em_rtc.o \
+../build/em_system.o \
+../build/em_usart.o \
+../build/em_vcmp.o \
+../build/gpio_constants.o \
+../build/init.o \
+../build/o2.o \
+../build/segmentlcd.o \
+../build/startup_efm32gg.o \
+../build/sys-tick_constants.o \
+../build/system_efm32gg.o \
+../build/vddcheck.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
--- o2/o2.s
+++ o2/o2.s
@@ -21,19 +21,37 @@ Start:
 
 	ldr r0, =SYSTICK_BASE
 	ldr r1, [r0]
-	orr r1, r1, #0b111
+	orr r1, r1, #0b110
+	str r1, [r0]
+
+	//Setter opp GOIO-button interrupt
+	ldr r0, =GPIO_BASE + GPIO_EXTIPSELH
+	ldr r1, [r0]
+	and r1, ~(0b1111 << 4)
+	orr r1, 0b0001 << 4
+	str r1, [r0]
+
+	ldr r0, =GPIO_BASE+GPIO_EXTIFALL
+	ldr r1, [r0]
+	orr r1, 1 << BUTTON_PIN
+	str r1, [r0]
+
+	ldr r0, =GPIO_BASE+GPIO_IEN
+	ldr r1, [r0]
+	orr r1, 1 << BUTTON_PIN
 	str r1, [r0]
 
 
-loop:
-	wfi
 
+loop:
+
+	wfi //'wait for interrupt'
 	B loop
+
 
 
 .global SysTick_Handler
 .thumb_func
-
 
 SysTick_Handler:
 
@@ -41,7 +59,6 @@ SysTick_Handler:
 	ldr r0, =tenths
 	ldr r1, [r0]
 	add r1, #1
-
 
 	cmp r1, #10		//Sjekker om tiendedelssekunder er blitt satt til 10
 	bne Tenths
@@ -52,10 +69,16 @@ SysTick_Handler:
 	ldr r2, =seconds
 	ldr r3, [r2]
 	add r3, #1
+	//Får her LED-en til å blinke:
+	ldr r7, =GPIO_BASE + LED_PORT * PORT_SIZE + GPIO_PORT_DOUTTGL //Finner porten
+	ldr r8, =1<<LED_PIN //setter led høg
+	str r8, [r7]
+
 
 	cmp r3, #60 //Skjekker om dete er gått 60 sek
 	bne Seconds
 	ldr r3, =0
+
 
 //Legger til ein i minutes og endrer lagrer verdien
 	ldr r5, =minutes
@@ -71,6 +94,25 @@ Seconds:
 Tenths:
 	str r1, [r0] //lagrer den nye verdien i tenths
 	bx lr
+
+
+.global GPIO_ODD_IRQHandler
+.thumb_func
+
+GPIO_ODD_IRQHandler:
+	// Toggler klokka
+	ldr r0, =SYSTICK_BASE
+	ldr r1, [r0]
+	eor r1, #SysTick_CTRL_ENABLE_Msk
+	str r1, [r0]
+
+	// Reset interrupt flag
+	ldr r0, =GPIO_BASE+GPIO_IFC //Loader clear register
+	ldr r1, =1<<BUTTON_PIN
+	str r1, [r0]
+
+	bx lr
+
 
 
 
